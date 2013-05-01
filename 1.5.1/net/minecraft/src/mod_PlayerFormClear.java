7914ed8c2e3aaf6d3fe7cdede9e6e610fa4ded5b
Modchu <kawd82jstskl@yahoo.co.jp>
2013-05-02 04:52:35 +0900

    新規

---
+++ 1.5.1/net/minecraft/src/mod_PlayerFormClear.java
@@ -0,0 +1,18 @@
+package net.minecraft.src;
+
+import java.util.Map;
+
+public class mod_PlayerFormClear extends BaseMod {
+
+	public String getVersion() {
+		return "1.5.1-1";
+	}
+
+	public void load() {
+	}
+
+	public void addRenderer(Map map) {
+		// プレーヤーフォルムの置き換え
+	    map.put(EntityPlayerSP.class, new RenderPlayerFormClear());
+	}
+}

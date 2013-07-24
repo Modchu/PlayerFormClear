ff824369f8fe212631b7e638b4fbe9cd74a0927f
modchu <kawd82jstskl@yahoo.co.jp>
2013-07-25 02:01:30 +0900

    1.6.2

---
+++ 1.6.2/net/minecraft/src/RenderPlayerFormClear.java
@@ -0,0 +1,19 @@
+package net.minecraft.src;
+
+public class RenderPlayerFormClear extends RenderPlayer {
+
+	public RenderPlayerFormClear() {
+		super();
+	}
+
+	@Override
+	public void doRender(Entity par1Entity, double par2, double par4, double par6, float par8, float par9)
+	{
+	}
+
+	@Override
+	public void renderFirstPersonArm(EntityPlayer par1EntityPlayer)
+	{
+	}
+
+}
